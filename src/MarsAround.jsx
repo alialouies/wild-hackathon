37571d325721b29faf1fadc6db740a06e93e4d1f
alialouies <makarona.aliki@gmail.com>
2021-05-11 17:39:47 +0200

    creating restaurant

---
+++ src/MarsAround.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import MarsAroundHeader from './MarsAroundHeader'
+import Products from './Products'
+import './MarsAround.css'
+
+
+function MarsAround() {
+    
+
+    return (
+        <>
+        <div id="main">
+            <MarsAroundHeader/>
+            <Products/>
+        </div>
+        </>
+    )
+}
+
+export default MarsAround
