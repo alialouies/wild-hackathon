78e69b2529a86b163661a73ab9e75119eb9677c5
EugeniuZaharia <78173175+EugeniuZaharia@users.noreply.github.com>
2021-05-11 16:29:41 +0200

    base game

---
+++ src/components/Clicker.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Clicker({ incrementCurrency }) {
+  return (
+    <div className="Clickers">
+
+      <div className="mushroom-button" onClick={incrementCurrency} />
+      <h2>Mushrooms</h2>
+      <br></br>
+      <div className="water-button" onClick={incrementCurrency} />
+      <h2>Water</h2>
+    </div>
+  );
+}
