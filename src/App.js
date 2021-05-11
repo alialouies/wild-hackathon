8255acb191dc46fa0ff81216b55989b8fd8c1edc
Lukasz Ospi <ospi@tlen.pl>
2021-05-11 22:02:19 +0200

    clicker game linked and water icon fixed on click

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
 import MarsAround from "./components/MarsAround";
 import Home from "./components/Home";
 import CalculatorHome from "./components/calculator/CalculatorHome";
+import GameClicker from "./components/clicker/GameClicker";
 
 function App() {
   return (
@@ -16,6 +17,9 @@ function App() {
           </Route>
           <Route path="/restaurant">
             <MarsAround />
+          </Route>
+          <Route path="/GameClicker">
+            <GameClicker />
           </Route>
         </Switch>
       </BrowserRouter>
