25044d935738f52e19202d97607a4580609e5e66
Angel Sámano Hamud <Angelsam@air-de-angel.localdomain>
2021-05-11 14:42:23 +0200

    Nav-Bar

--- src/App.js
+++ src/App.js
@@ -1,37 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Header, Nav, Link, SvgBox } from "./components/Styles.js";
+
+import "./App.css";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const iconVariants = {
+    opened: {
+      rotate: 135
+    },
+    closed: {
+      rotate: 0
+    }
+  };
+
+  const menuVariants = {
+    opened: {
+      top: 0,
+      transition: {
+        when: "beforeChildren",
+        staggerChildren: 0.5
+      }
+    },
+    closed: {
+      top: "-80vh"
+    }
+  };
+
+  const linkVariants = {
+    opened: {
+      opacity: 1,
+      y: 50
+    },
+    closed: {
+      opacity: 0,
+      y: 0
+    }
+  };
+
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/game">Game</Link>
-            </li>
-            <li>
-              <Link to="/weather">Weather</Link>
-            </li>
-          </ul>
-        </nav>
-        <Switch>
-          <Route path="/">
-            {/* <Home /> */}
-          </Route>
-          <Route path="/game">
-            {/* <Game /> */}
-          </Route>
-          <Route path="/weather">
-            {/* <Weather /> */}
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <div className="App">
+      <Header>
+        <SvgBox
+          variants={iconVariants}
+          animate={isOpen ? "opened" : "closed"}
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M12 4C11.4477 4 11 4.44772 11 5V11H5C4.44772 11 4 11.4477 4 12C4 12.5523 4.44772 13 5 13H11V19C11 19.5523 11.4477 20 12 20C12.5523 20 13 19.5523 13 19V13H19C19.5523 13 20 12.5523 20 12C20 11.4477 19.5523 11 19 11H13V5C13 4.44772 12.5523 4 12 4Z"
+              fill="#fff"
+            />
+          </svg>
+        </SvgBox>
+      </Header>
+
+      <Nav
+        initial={false}
+        variants={menuVariants}
+        animate={isOpen ? "opened" : "closed"}
+      >
+        <Link variants={linkVariants}>Planet</Link>
+        <Link variants={linkVariants}>Life</Link>
+        <Link variants={linkVariants}>Map</Link>
+      </Nav>
+    </div>
   );
 }
 
---
+++ src/components/Styles.js
@@ -0,0 +1,35 @@
+import { motion } from "framer-motion";
+import styled from "styled-components";
+
+export const Header = styled.header`
+  background: black;
+  position: relative;
+  display: flex;
+  padding: 1rem;
+  justify-content: flex-end;
+  z-index: 2;
+`;
+
+export const Nav = styled(motion.nav)`
+  background-color: orange;
+  height: 90vh;
+  width: 100vw;
+  position: fixed;
+  top: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+
+
+export const Link = styled(motion.li)`
+  color: white;
+  margin-bottom: 1.6rem;
+  font-size: 1.4rem;
+`;
+
+export const SvgBox = styled(motion.div)`
+
+`;
