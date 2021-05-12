5c796807845431513de554970ea30ed91ef126ac
alialouies <makarona.aliki@gmail.com>
2021-05-12 09:57:14 +0200

    fixing navigation

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -10,30 +10,30 @@ const Menu = () => {
   return (
     <div className="home">
       <div id="menu-container">
-        <button className="menu-text" onClick={history.push("/")}>
+        <button className="menu-text" onClick={() => history.push("/")}>
           Home
         </button>
-        <button className="menu-text" onClick={history.push("/GameClicker")}>
+        <button className="menu-text" onClick={() => history.push("/GameClicker")}>
           Gain Money
         </button>
         <br></br>
-        <button className="menu-text" onClick={history.push("/menu")}>
+        <button className="menu-text" onClick={() => history.push("/menu")}>
           Play Game
         </button>
         <br></br>
-        <button className="menu-text" onClick={history.push("/calculator")}>
+        <button className="menu-text" onClick={() => history.push("/calculator")}>
           Calculate Mars Age
         </button>
         <br></br>
-        <button className="menu-text" onClick={history.push("/restaurant")}>
+        <button className="menu-text" onClick={() => history.push("/restaurant")}>
           Shop
         </button>
         <br></br>
-        <button className="menu-text" onClick={history.push("/killearth")}>
+        <button className="menu-text" onClick={() => history.push("/killearth")}>
           Kill Earth game
         </button>
         <br></br>
-        <button className="menu-text" onClick={history.push("/weather")}>
+        <button className="menu-text" onClick={() => history.push("/weather")}>
           Mars Weather
         </button>
         <br></br>
