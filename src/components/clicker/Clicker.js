972bb8d69cdceae504852c7ea06e4e18db29d66b
alialouies <makarona.aliki@gmail.com>
2021-05-12 11:32:48 +0200

    adding sound

--- src/components/clicker/Clicker.js
+++ src/components/clicker/Clicker.js
@@ -1,14 +1,20 @@
 import React from "react";
+import Sound from '../../assets/ka-ching.mp3'
 
 export default function Clicker({ incrementCurrency }) {
+  const audioA = new Audio(Sound);
+
+  const playSound = () => {
+    audioA.play()
+  }
   return (
     
     <div className="Clickers">
      
-      <div className="mushroom-button" onClick={incrementCurrency} />
+      <div className="mushroom-button" onClick={() => {incrementCurrency(); playSound()}} />
       <h3>Mushrooms</h3>
       <br></br>
-      <div className="water-button" onClick={incrementCurrency} />
+      <div className="water-button" onClick={() => {incrementCurrency(); playSound()}} />
       <h3>Water</h3>
     </div>
   );
